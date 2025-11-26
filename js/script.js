f237369515c70dce7296ed2fbc4635a7883f1e74
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2025-11-26 20:38:58 -0300

    final5

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ if (formLogin) {
     if (usuario && usuario.senha === senha) {
       alert(`Bem-vindo(a), ${usuario.nome}!`);
       localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
-      window.location.href = 'dashboard.html';
+      window.location.href = 'index.html';
     } else {
       alert('Email ou senha incorretos!');
     }
